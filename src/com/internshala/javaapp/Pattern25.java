28a7b31e7337234605e84e58b4aa1d13038a296a
Dheeraj Patel <dheeraj162001@gmail.com>
2022-12-04 11:22:52 +0530

    All Pattern Question Practice...

---
+++ src/com/internshala/javaapp/Pattern25.java
@@ -0,0 +1,20 @@
+package com.internshala.javaapp;
+
+public class Pattern25 {
+    static void pattern(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                if (row == 1 || row == n || col == 1 || col == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern(7);
+    }
+}
